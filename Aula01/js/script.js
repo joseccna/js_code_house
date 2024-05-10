fef748c4e333d3f4c6a100717068920aed204852
José Fernando <jose.ccna@gmail.com>
2024-05-09 21:10:01 -0300

    criando pagina

---
+++ Aula01/js/script.js
@@ -0,0 +1,53 @@
+
+
+//  var numeroA = 1;
+//  var numeroB = 2;
+//  var numeroC = 3;
+//  //Soma de dois números
+//  var resultadoSoma = numeroA + numeroB;
+//  //Subtração de dois números
+//  var resultadoSubtracao = numeroB - numeroA;
+//  //Produto de dois números 
+//  var resultadoProduto = numeroB * numeroC;
+//  //Divisão de dois números
+//  var resultadoDivisao = numeroC / numeroB;
+
+
+
+//  var textoA = "CODER";
+//  var textoB = "HOUSE";
+//  var branco = " ";
+//  //Concatenar textoA e texto B
+//  var resultadoA = textoA + textoB;
+//  //Concatenar textoB e 1 
+//  var resultadoB = textoB + 1;
+//  //Concatenar textoA, branco e textoB
+//  var resultadoC = textoA + branco +
+// textoB;
+
+// var nome = 'José Fernando';
+// var sobrenome = ' Nascimento Carvalho';
+// var ano = 2024
+// var idade = 43
+// var nomeCompleto = nome + sobrenome;
+// var resuldoIdade = ano - idade;
+
+// var numero = prompt('Insira um numero: ')
+
+// console.log('sou jose');
+// console.log(seunome);
+// alert('sou jose ' + seunome)
+
+
+// var resultado = parseInt(numero) + 10;
+// alert(resultado)
+
+
+nu1 = prompt('Digite um numero: ');
+nu2 = prompt('Digiete o segundo numero: ');
+
+soma = parseInt(nu1) + parseInt(nu2);
+console.log(soma);
+
+produto = parseInt(nu1) * parseInt(nu2);
+alert(produto);
