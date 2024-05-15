f90f71a3d718f20535b7477768d6c7a723689636
José Fernando <jose.ccna@gmail.com>
2024-05-14 21:54:28 -0300

    terminado a aula de condicional.

---
+++ Aula02_condicionais_if/atividade/pratica1.js
@@ -0,0 +1,20 @@
+numero = prompt('Digitte um numero de 1 a 5: ');
+
+if(numero == '1'){
+    alert('numero um.');
+}
+else if(numero == '2'){
+    alert('numero dois.');
+}
+else if(numero == '3'){
+    alert('numero tres.');
+}
+else if(numero == '4'){
+    alert('numero quatro.');
+}
+else if(numero == '5'){
+    alert('numero cinco.');
+}
+else{
+    alert('numero errado!');
+}
---
+++ Aula02_condicionais_if/condicional.js
@@ -0,0 +1,110 @@
+// var idade = 50
+
+
+// if( idade == 10){
+// console.log('Primeria condição é verdadeira');
+// }
+// if(idade == 20){
+//     console.log('Segunda condição é verdadeira');
+// }
+// if(idade == 30){
+//     idade += 10;
+//     console.log(idade);
+// }
+// else{
+//     console.log('Deu errado rsrs')
+// }
+
+/////////////////////////////////////////////////////////////////////
+
+// var combustivel = 'Gasolina';
+// if (combustivel == 'gnv'){
+//     alert('movido a gas');
+// }
+
+// else if (combustivel == "Gasolina") {
+//     alert("O carro é movido à Gasolina");
+//    }
+//    else if (combustivel == "Álcool") {
+//     alert("O carro é movido à Álcool");
+//    }
+//    else {
+// //     alert("O carro não funciona");
+//    }
+
+   ////////////////////////////////////////////////////////////
+
+//    "declaração de valores boleano"
+//    var boleana = true;
+//    var boleana2 = false;
+
+// var idade = 10;
+// var possui10anos = (idade == 10); // seu valor será true
+// if (possui10anos) {
+//  alert("Essa pessoa possui 10 anos de idade");
+//  console.log(possui10anos);
+// }
+   
+
+//////////////////////////////////////////////////////////////
+
+// var valor = "ana";
+// if (valor) {
+//  alert("Valor verdadeiro")
+// }
+// else {
+//  alert("Valor falso")
+// }
+
+/////////////////////////////////////////////////////////////
+
+// Operadores
+
+// var numero1 = 10;
+// var numero2 = 20;
+// var numero3 = 30;
+
+// if ( numero2 >! 50 ){
+//     console.log('verdadeiro')
+// }
+// else{
+//     console.log('Falso')}
+
+////////////////////////////////////////////////////////
+
+// var nome = prompt("Inserir nome");
+// if ((nome != "") && ((nome == "EMA") || (nome == "ema") ||
+// (nome == "Ema"))) {
+//  alert("Olá, Ema");
+// } else {
+//  alert("Erro: Inserir nome válido");
+// }
+/////////////////////////////////////////////////////////////////////
+
+// ATIVIDADE EXTRA
+// Criar um algoritmo com uma condicional
+
+// Descrição
+// ✓ Crie um algoritmo que solicite ao 
+// usuário um ou mais valores inseridos 
+// por prompt(),
+// ✓ Compare as entradas 
+// ✓ Mostre o resultado por console ou 
+// alert() de acordo com os valores 
+// inseridos e as condições cumpridas.
+// Observação
+// ✓ Não há entrega dessa atividade.
+
+// Exemplos
+// ✓ Solicitar um número através do prompt 
+// e, se for maior que 1000, mostrar um 
+// alert.
+// ✓ Solicitar um texto através de prompt e, 
+// se for igual a “Olá“, mostrar uma 
+// mensagem no console.
+// ✓ Solicitar um número através do prompt 
+// e verificar se está entre 10 e 50. Em 
+// caso positivo, mostrar um alert.
+// Formato
+// ✓ Formato indicado pelo professor. Nome 
+// + Sobrenome.
