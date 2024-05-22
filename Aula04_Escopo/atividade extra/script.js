26338b8a0e4acd95b20f9e93a2d2f7ca43a982ae
José Fernando <jose.ccna@gmail.com>
2024-05-22 16:57:36 -0300

    inicio da atividade extra

---
+++ Aula04_Escopo/atividade extra/script.js
@@ -0,0 +1,7 @@
+function receber(){
+    let primeiro = document.getElementById("primeiro");
+    console.log(primeiro.value);
+
+    let segundo = document.getElementById("segundo");
+    console.log(segundo.value);
+}
